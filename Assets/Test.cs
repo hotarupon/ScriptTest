2ccbf10e294a3dcfd57e9794868a2dc7451aff62
mizuki.suzuki <htayhtjy38@gmail.com>
2022-05-20 13:47:12 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        int[] array = {10,20,30,40,50};
+
+        for (int i = 0; i < 5; i++)
+
+        {
+            Debug.Log(array[i]);
+
+        }
+
+        for (int i = 4; i > -1; i--)
+
+            {
+            Debug.Log(array[i]);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
